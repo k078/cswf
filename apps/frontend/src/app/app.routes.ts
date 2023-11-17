43c06a4c4d251b94d4b9f1d9be3671708bf8b6d8
k078 <77967048+k078@users.noreply.github.com>
2023-11-17 20:10:20 +0100

    Deployment nog steeds

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [];
+import { Routes } from '@angular/router';
+import { VerzamelingListComponent } from 'libs/frontend/features/src/lib/verzameling/verzameling-list/verzameling-list.component';
+import { VerzamelingDetailComponent } from 'libs/frontend/features/src/lib/verzameling/verzameling-detail/verzameling-detail.component';
+export const appRoutes: Routes = [
+  { path: '', component: VerzamelingListComponent },
+  { path: 'verzameling/:id', component: VerzamelingDetailComponent },
+  // Voeg hier andere routes toe
+];
