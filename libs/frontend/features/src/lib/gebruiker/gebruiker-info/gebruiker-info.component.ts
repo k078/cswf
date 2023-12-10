58040dd643109a595ed42921e099416cae4f7f16
k078 <77967048+k078@users.noreply.github.com>
2023-12-10 22:45:04 +0100

    Login + registratie edit

--- libs/frontend/features/src/lib/gebruiker/gebruiker-info/gebruiker-info.component.ts
+++ libs/frontend/features/src/lib/gebruiker/gebruiker-info/gebruiker-info.component.ts
@@ -3,6 +3,7 @@ import { GebruikerService } from '../gebruiker.service';
 import { ILp, IGebruiker } from '@cswf/shared/api';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { VerzamelingService } from '../../verzameling/verzameling.service';
 
 @Component({
   selector: 'cswf-gebruiker-info',
@@ -10,22 +11,23 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./gebruiker-info.component.css'],
 })
 export class GebruikerInfoComponent implements OnInit {
-  Gebruiker: IGebruiker | null = null;
+  gebruiker: IGebruiker | null = null;
   lps: ILp[] | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private GebruikerService: GebruikerService,
+    private gebruikerService: GebruikerService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private verzamelingService: VerzamelingService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
-        this.GebruikerService.read(id).subscribe((Gebruiker) => {
-          this.Gebruiker = Gebruiker;
+        this.gebruikerService.read(id).subscribe((gebruiker) => {
+          this.gebruiker = gebruiker;
         });
       }
     });
@@ -33,7 +35,14 @@ export class GebruikerInfoComponent implements OnInit {
 
   verwijderGebruiker(id: number): void {
     this.authService.logout();
-    this.GebruikerService.delete(id).subscribe(
+    this.verzamelingService.list().subscribe((results) => {
+      results?.forEach((verzameling) => {
+        if (verzameling.eigenaar === this.gebruiker?.gebruikersnaam) {
+          this.verzamelingService.verwijderVerzameling(verzameling.id).subscribe();
+        }
+      });
+    });
+    this.gebruikerService.delete(id).subscribe(
       () => {
         console.log('Gebruiker successfully deleted');
         this.router.navigate(['/']);
